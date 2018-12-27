75618c38b449f8ea4ffbaec1abfafa41f2159fd0
Trys <trysmudford@gmail.com>
2018-12-27 09:12:41 +0000

    Deploy readiness

--- preact.config.js
+++ preact.config.js
@@ -1,5 +1,19 @@
 import envVars from 'preact-cli-plugin-env-vars';
+import netlifyPlugin from 'preact-cli-plugin-netlify';
 
 export default function(config, env, helpers) {
+  if (env.isProd) {
+    // Make async work
+    let babel = config.module.loaders.filter(
+      loader => loader.loader === 'babel-loader'
+    )[0].options;
+    // Blacklist regenerator within env preset:
+    babel.presets[0][1].exclude.push('transform-async-to-generator');
+    // Add fast-async
+    babel.plugins.push([require.resolve('fast-async'), { spec: true }]);
+  }
+
+  netlifyPlugin(config);
+
   envVars(config, env, helpers);
 }
