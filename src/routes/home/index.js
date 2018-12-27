0af6d3f16528fcae27e12bc49c1b23c56cc2a81a
Trys <trysmudford@gmail.com>
2018-12-27 14:27:01 +0000

    Copy

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -5,14 +5,38 @@ import { url } from '../../utils/date';
 const today = url(new Date());
 const onboarded = !!localStorage.getItem('journalbook_onboarded');
 
-const Home = () => (
-  <div class="wrap center home">
-    <h1>Welcome to your Journal</h1>
-    <img src="/assets/images/study.svg" alt="" />
-    <Link href={onboarded ? today : '/get-started/'} class="button">
-      Start writing
-    </Link>
-  </div>
-);
+const Home = () =>
+  onboarded ? (
+    <div class="wrap center home">
+      <h1>This is your JournalBook</h1>
+      <img src="/assets/images/study.svg" alt="" />
+      <Link href={today} class="button">
+        Start writing
+      </Link>
+    </div>
+  ) : (
+    <div class="wrap wrap--thin center home">
+      <h1>Welcome to JournalBook</h1>
+      <img src="/assets/images/study.svg" alt="" />
+      <Link href="/get-started/" class="button">
+        Start writing
+      </Link>
+      <br />
+      <h2>What's JournalBook?</h2>
+      <p>JournalBook is a private, offline-first personal journal.</p>
+      <p>
+        Your notes are <strong>only</strong> stored on your device, they're
+        never sent to any server or tracking facility. It's designed to work
+        offline, so you can reflect on your day whilst on the train journey
+        home.
+      </p>
+      <p>
+        It's quick, lightweight, and developed{' '}
+        <a href="https://github.com/trys/JournalBook">in the open</a>. You can
+        even add it to your homescreen as an app.
+      </p>
+      <p />
+    </div>
+  );
 
 export default Home;
