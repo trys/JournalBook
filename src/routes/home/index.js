0cb862230903c78baf3ae479e5310f70eff1db5d
Trys <trysmudford@gmail.com>
2018-12-27 08:21:18 +0000

    Home mobile styling

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -5,8 +5,12 @@ import { url } from '../../utils/date';
 const today = url(new Date());
 
 const Home = () => (
-  <div>
-    <Link href={today}>Today</Link>
+  <div class="wrap center">
+    <h1>Welcome to your Journal</h1>
+    <br />
+    <Link href={today} class="button">
+      Start writing
+    </Link>
   </div>
 );
 
