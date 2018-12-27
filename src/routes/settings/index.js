d825dadd6ce13847206e5b82e7f4978e50d8bc0f
Trys <trysmudford@gmail.com>
2018-12-27 08:29:03 +0000

    Mobile settings

--- src/routes/settings/index.js
+++ src/routes/settings/index.js
@@ -62,15 +62,10 @@ export default class Questions extends Component {
   render(props, { questions }) {
     return (
       <div class="wrap">
-        <h1>Settings</h1>
-
-        <form onSubmit={this.addQuestion}>
-          <input type="text" name="question" />
-          <input type="submit" value="Add question" />
-        </form>
+        <h1 class="center h1">Your Questions</h1>
 
         {questions.map(question => (
-          <div>
+          <p>
             <input
               type="text"
               value={question.question}
@@ -78,8 +73,30 @@ export default class Questions extends Component {
                 this.updateQuestion(question.slug, event.target.value)
               }
             />
-          </div>
+          </p>
         ))}
+
+        <form onSubmit={this.addQuestion} class="add-question">
+          <div>
+            <label for="add-question">Add a question</label>
+            <input
+              id="add-question"
+              type="text"
+              name="question"
+              placeholder="e.g. What are you grateful for?"
+            />
+          </div>
+          <button type="submit">
+            <svg width="14" height="14" xmlns="http://www.w3.org/2000/svg">
+              <title>Add Question</title>
+              <path
+                d="M13.13 6.13H7.87V.88C7.88.34 7.54 0 7 0s-.88.35-.88.88v5.25H.88C.35 6.13 0 6.46 0 7s.35.88.88.88h5.25v5.25c0 .52.34.87.87.87s.88-.35.88-.88V7.88h5.25c.52 0 .87-.35.87-.88s-.35-.88-.88-.88z"
+                fill="#4951A3"
+                fill-rule="nonzero"
+              />
+            </svg>
+          </button>
+        </form>
       </div>
     );
   }
