4828065e673dd2402a65874cf86ce5003f9d8369
Trys Mudford <trys@clearleft.com>
2019-11-27 20:53:36 +0000

    Word count

--- src/routes/stats/index.js
+++ src/routes/stats/index.js
@@ -13,6 +13,9 @@ class Stats extends Component {
     uniqueDates: 0,
     wordCount: 0,
     totalHighlights: 0,
+    popularWords: [],
+    showPopularWords: false,
+    showStopWords: false,
   };
 
   componentDidMount() {
@@ -33,20 +36,80 @@ class Stats extends Component {
       }
 
       // Prepare the keys
-      const keys = keysData.map(key => {
-        return Number(key.split('_').shift());
-      });
+      const keys = keysData.map(key => Number(key.split('_').shift()));
       keys.sort((a, b) => a - b);
 
       const unique = keys.reduce(this.removeDuplicates, []);
 
       // const dates = unique.map(this.keyToDate);
 
+      const theWords = {};
+      const stopWords = [
+        'i',
+        'a',
+        'and',
+        'the',
+        'in',
+        'of',
+        'to',
+        'then',
+        'they',
+        'was',
+        'for',
+        'with',
+        'it',
+        'we',
+        'on',
+        'an',
+        'but',
+        'at',
+        'had',
+        'so',
+        'went',
+        'up',
+        'some',
+        'got',
+        'my',
+        'be',
+        'that',
+        'this',
+        "i'm",
+        'made',
+        'out',
+        "it's",
+        'is',
+        'did',
+        'me',
+        'too',
+        'how',
+        'off',
+        'as',
+      ];
+
       // Word count
       const entries = await this.props.db.getAll('entries');
       const wordCount = entries.reduce((c, entry) => {
-        return c + entry.split(' ').length;
+        const words = entry
+          .split(/[.\s,]+/)
+          .map(x => x.toLowerCase())
+          .filter(Boolean);
+
+        words
+          .filter(x => this.state.showStopWords || !stopWords.includes(x))
+          .forEach(w => (theWords[w] = theWords[w] ? theWords[w] + 1 : 1));
+
+        return c + words.length;
       }, 0);
+
+      const highestValues = Array.from(
+        new Set(Object.values(theWords).sort((a, b) => b - a))
+      );
+      const popularWords = highestValues.slice(0, 50).map(v => [
+        v,
+        Object.keys(theWords)
+          .filter(k => theWords[k] === v)
+          .join(', '),
+      ]);
 
       // Highlights
       const highlights = await this.props.db.getAll('highlights');
@@ -57,6 +120,7 @@ class Stats extends Component {
         uniqueDates: unique.length,
         wordCount,
         totalHighlights,
+        popularWords,
       });
     } catch (e) {
       // console.error(e);
@@ -77,7 +141,18 @@ class Stats extends Component {
     return new Date(dateString);
   }
 
-  render({}, { totalEntries, uniqueDates, wordCount, totalHighlights }) {
+  render(
+    {},
+    {
+      totalEntries,
+      uniqueDates,
+      wordCount,
+      totalHighlights,
+      popularWords,
+      showPopularWords,
+      showStopWords,
+    }
+  ) {
     const stats = (
       <div>
         <p>
@@ -100,6 +175,43 @@ class Stats extends Component {
             <Link href="/highlights/">here</Link>
           </p>
         ) : null}
+
+        {showPopularWords && popularWords.length ? (
+          <div>
+            <p>Your most popular words are:</p>
+            <table class="left">
+              <tbody>
+                {popularWords.map(([v, k]) => (
+                  <tr key={k}>
+                    <th>{v}</th>
+                    <td class="capitalize">{k}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <br />
+            {!showStopWords ? (
+              <button
+                type="button"
+                class="button button--grey"
+                onClick={() => {
+                  this.setState({ showStopWords: true });
+                  this.getData();
+                }}
+              >
+                Include stop words
+              </button>
+            ) : null}
+          </div>
+        ) : (
+          <button
+            type="button"
+            class="button button--grey"
+            onClick={() => this.setState({ showPopularWords: true })}
+          >
+            Show popular words
+          </button>
+        )}
         <p>Well done! 👏</p>
       </div>
     );
