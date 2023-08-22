33680a3c851e8bbce8a3e80418adfa1ed4d40782
Trys Mudford <trys@clearleft.com>
2023-08-22 15:20:41 +0100

    feat: simpler accept

--- src/routes/settings/index.js
+++ src/routes/settings/index.js
@@ -322,7 +322,7 @@ class Settings extends Component {
             class="screen-reader-only"
             id="import"
             onChange={this.importData}
-            // accept=".json,application/json"
+            accept=".json"
           />
           <label for="import" class="button button--grey">
             {importing ? 'Importing...' : 'Import'}
